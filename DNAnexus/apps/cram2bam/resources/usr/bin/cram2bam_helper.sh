7a06820fd648134d0cde987f29354ae37842b321
BJWiley233 <wileyb@wustl.edu>
2021-07-27 23:42:10 -0500

    alot

---
+++ DNAnexus/apps/cram2bam/resources/usr/bin/cram2bam_helper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ref_fasta_path=$1
+cram_in_path=$2
+bam_out=$3
+
+/usr/local/bin/samtools view -b -T $ref_fasta_path $cram_in_path -o $bam_out;
+/usr/local/bin/samtools index $bam_out;
---
+++ DNAnexus/apps/qc_exome_ukbb/resources/usr/bin/qc_helper.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export bam_path=$1; shift; 
+export reference_path=$1; shift; 
+export reference_index_path=$1; shift;
+export picard_metric_accumulation_level=$1; shift; 
+echo $picard_metric_accumulation_level
+export bait_intervals_path=$1; shift; 
+echo $bait_intervals_path
+export target_intervals_path=$1; shift; 
+export per_target_coverage=$1; shift; 
+export per_base_coverage=$1; shift; 
+export minimum_mapping_quality=$1; shift; 
+export minimum_base_quality=$1; shift; 
+export alignment_summary_metrics=$1; shift; 
+export hs_metrics=$1;
+
+/usr/local/openjdk-8/bin/java -Xmx16g -jar /usr/picard/picard.jar CollectAlignmentSummaryMetrics \
+    INPUT=$bam_path \
+    REFERENCE_SEQUENCE=$reference_path \
+    METRIC_ACCUMULATION_LEVEL=$picard_metric_accumulation_level \
+    OUTPUT=$alignment_summary_metrics
+
+echo "
+/usr/local/openjdk-8/bin/java -Xmx16g -jar /usr/picard/picard.jar CollectHsMetrics \
+    -I $bam_path \
+    -R $reference_path \
+    -METRIC_ACCUMULATION_LEVEL ALL_READS \
+    -BI $bait_intervals_path \
+    -TI $target_intervals_path \
+    -PER_TARGET_COVERAGE $per_target_coverage \
+    -PER_BASE_COVERAGE $per_base_coverage \
+    -MINIMUM_MAPPING_QUALITY $minimum_mapping_quality \
+    -MINIMUM_BASE_QUALITY $minimum_base_quality \
+    -O $hs_metrics
+"
+/usr/local/openjdk-8/bin/java -Xmx16g -jar /usr/picard/picard.jar CollectHsMetrics \
+    -I $bam_path \
+    -R $reference_path \
+    -METRIC_ACCUMULATION_LEVEL ALL_READS \
+    -BI $bait_intervals_path \
+    -TI $target_intervals_path \
+    -PER_TARGET_COVERAGE $per_target_coverage \
+    -PER_BASE_COVERAGE $per_base_coverage \
+    -MINIMUM_MAPPING_QUALITY $minimum_mapping_quality \
+    -MINIMUM_BASE_QUALITY $minimum_base_quality \
+    -O $hs_metrics
