cb10c5fe71d0c82852a7ba77de5741b857bea6c1
BJWiley233 <wileyb@wustl.edu>
2021-08-13 16:32:10 -0500

    msk pileup + vep2

--- DNAnexus/apps/vardict/resources/usr/local/bin/VardictJava.sh
+++ DNAnexus/apps/vardict/resources/usr/local/bin/VardictJava.sh
@@ -9,6 +9,6 @@ normal_bam="$6"
 normal_sample_name="$7"
 out="$8"
 
-/opt/VarDictJava/build/install/VarDict/bin/VarDict -G $REF -f $AF_THR -N $tumor_sample_name -b "$tumor_bam|$normal_bam" -c 1 -S 2 -E 3 -g 4 $bed | /opt/VarDictJava/build/install/VarDict/bin/testsomatic.R | /opt/VarDictJava/build/install/VarDict/bin/var2vcf_paired.pl -N "$tumor_sample_name|$normal_sample_name" -f $AF_THR > $out
+/opt/VarDictJava/build/install/VarDict/bin/VarDict -G $REF -f $AF_THR -N $tumor_sample_name -b "$tumor_bam|$normal_bam" -c 1 -S 2 -E 3 -g 4 $bed -th 4 | /opt/VarDictJava/build/install/VarDict/bin/testsomatic.R | /opt/VarDictJava/build/install/VarDict/bin/var2vcf_paired.pl -N "$tumor_sample_name|$normal_sample_name" -f $AF_THR > $out
 
 bgzip $out && tabix $out.gz
