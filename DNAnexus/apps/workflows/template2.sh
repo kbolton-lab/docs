bf9a5d7f2441617c4a4a8c6cea31091dec49c70d
BJWiley233 <wileyb@wustl.edu>
2021-08-25 11:31:03 -0500

    workflows again

--- DNAnexus/apps/workflows/template2.sh
+++ DNAnexus/apps/workflows/template2.sh
@@ -4,7 +4,7 @@ wf_folder=$3
 cram=$(dx find data --class file --path UKBB_Exome_2021:"/Bulk/Exome sequences/Exome OQFE CRAM files/$folder_prefix" --json --name ${eid}_23153_0_0.cram | jq -c '.[].describe.id')
 cram_index=$(dx find data --class file --path UKBB_Exome_2021:"/Bulk/Exome sequences/Exome OQFE CRAM files/$folder_prefix" --json --name ${eid}_23153_0_0.cram.crai | jq -c '.[].describe.id')
 
-/Users/brian/Bolton/UKBB/docs/DNAnexus/apps/cram_in2.py $eid $cram $cram_index $wf_folder
+/Users/brian/Bolton/UKBB/docs/DNAnexus/apps/cram_in2.py $eid $cram $cram_index $wf_folder $folder_prefix
 
 #/Users/brian/Bolton/UKBB/docs/DNAnexus/apps/workflows/Mutect_Vardict_CH_NO_BQSR
 
